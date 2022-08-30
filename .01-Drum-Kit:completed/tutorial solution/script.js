72cd23ff75fdb38cbc6a80901e0029e26ff88d08
cryptoretard25 <mikhmikhlobanov@gmail.com>
2022-08-30 20:12:00 +0300

    Drum-kit completed

--- .01-Drum-Kit:completed/tutorial solution/script.js
+++ .01-Drum-Kit:completed/tutorial solution/script.js
@@ -4,17 +4,18 @@ window.addEventListener("keydown", renderKeyDown);
 const keys = document.querySelectorAll("div.key");
 
 // Listening to Transition End event
-// CSS transitions provide a way to control animation speed when changing CSS properties. 
-// Instead of having property changes take effect immediately, you can cause the changes in a 
+// CSS transitions provide a way to control animation speed when changing CSS properties.
+// Instead of having property changes take effect immediately, you can cause the changes in a
 // property to take place over a period of time.
-// We wanna delete animation class, right after transition ends 
+// We wanna delete animation class, right after transition ends
 keys.forEach((key) => {
   key.addEventListener("transitionend", removeTransition);
 });
+
 function removeTransition(event) {
   if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
-
 
 function renderKeyDown(event) {
   const div = document.querySelector(`.key[data-key="${event.key}"]`);
@@ -25,4 +26,3 @@ function renderKeyDown(event) {
 
   div.classList.toggle("playing");
 }
-
